6536557bc61c0d7cd7f73f4a760e9edfe6f0c133
aeskay <alaladesambluez@gmail.com>
2020-07-05 12:41:20 +0100

    This is the first commit

---
+++ responsive-menu.js
@@ -0,0 +1,11 @@
+var icon = document.getElementById("icon");
+var links = document.getElementById("navigation");
+
+
+
+function tester(){
+	document.getElementById("navigation").classList.toggle("active");
+	document.getElementById("faicon").classList.toggle("fa fa-times");
+}
+
+
